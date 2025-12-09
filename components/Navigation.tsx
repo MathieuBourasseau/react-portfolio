c290b13ffaa41dae46988b567d0825ca5c51174a
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-09 17:24:02 +0100

    feat: project initialized + create the component footer

---
+++ components/Navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+   <header>
+
+        <nav>
+
+            {/* BLOC LOGO */}
+            <div>
+                <p>Mon image</p>
+                <p>Mathieu Bourasseau</p>
+            </div>
+
+            {/* BLOC Navigation */}
+            <div>
+            </div>
+        </nav>
+
+   </header>
+  );
+}
