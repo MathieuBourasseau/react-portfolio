8c647c4932c7829e0f9aba1858f73e9825b46058
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-13 15:43:11 +0100

    footer style is responsive + span tag replaced by links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,24 +2,34 @@ import React from 'react'
 import { FaGithub } from "react-icons/fa"
 import { IoIosMail } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
 const socialsIcons = [
-  { label: "Github", icon: <FaGithub /> },
-  { label: "LinkedIn", icon: <FaLinkedin /> },
-  { label: "Contact", icon: <IoIosMail />}
+    { label: "Github", icon: <FaGithub />, url: "https://github.com/MathieuBourasseau" },
+    { label: "LinkedIn", icon: <FaLinkedin />, url: "https://www.linkedin.com/in/mathieu-bourasseau/" },
+    { label: "Contact", icon: <IoIosMail />, url: "#" }
 ]
 
 export default function Footer() {
-  return (
-    <footer className=" flex flex-col gap-4 bg-[url(/header/background.jpg)] p-6 text-white">
-      <div>
-        <p className="text-sm">© 2025 Mathieu Bourasseau – Tous droits réservés</p>
-      </div>
-      <div className="flex gap-4">
-        <p className="text-sm">Contact</p>
-        <span>1</span>
-      </div>
-
-    </footer>
-  )
+    return (
+        <footer className="flex flex-col items-center bg-[url(/header/background.jpg)] p-6 text-white">
+            <div className="flex flex-col items-center gap-6 lg:flex-row lg:justify-between lg:max-w-[1000px] lg:mx-auto lg:w-full">
+                <div>
+                    <p className="text-sm text-center md:text-base lg:text-xl">© 2025 Mathieu Bourasseau – Tous droits réservés</p>
+                </div>
+                <div className="flex gap-4 items-center">
+                    <p className="text-sm md:text-base lg:text-xl">Contact</p>
+                    {socialsIcons.map((social, i) => (
+                        <Link
+                            key={i}
+                            href={social.url}
+                            target={social.url.startsWith('http') ? "_blank" : undefined}
+                        >
+                            <span className="text-[20px] lg:text-3xl">{social.icon}</span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    )
 }
