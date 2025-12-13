f4ee2692b0eaa9473101e75568f7c8a46d437d34
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-13 17:07:00 +0100

    feat: list of links added + beginning of the style added

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -4,6 +4,8 @@ import { FaComputer } from "react-icons/fa6";
 import { IoIosMail } from "react-icons/io";
 import { FaPlane } from "react-icons/fa";
 import { MdWork } from "react-icons/md";
+import { IoMdArrowDropdown } from "react-icons/io";
+import Link from "next/link";
 
 type itemsConfig = {
     icon: string;
@@ -58,31 +60,61 @@ export default function HomePage() {
                     <p className="text-sm">Ce site web a pour objectif de vous présenter mon parcours, mes compétences, ainsi que les projets que j’ai pu réalisés.</p>
                     <p className="text-sm">En espérant pouvoir se rencontrer prochainement, je vous souhaite une très bonne découverte de mon portfolio.</p>
                 </div>
-                <div>
+
+                {/* RIGHT SIDE CONTENT */}
+
+                <div className=" flex flex-col items-center gap-4 rounded-xl py-4 px-5 bg-[url(/header/background.jpg)]">
                     <Image
                         src={`/intro/logo-avatar-mathieu.png`}
-                        width={35}
-                        height={35}
+                        width={60}
+                        height={60}
                         alt="Avatar de Mathieu Bourasseau"
                     />
 
-                    <div>
-                        <p className="text-sm">Mathieu Bourasseau</p>
-                        <span className="text-sm font-bold text-[#F74518]">Recherche une alternance de concepteur et développeur d'application</span>
-                        <p className="text-sm">"Test, fail, learn and repeat !"</p>
+                    <div className="">
+                        <p className="text-sm text-white font-bold text-center">Mathieu Bourasseau</p>
+                        <span className="text-sm font-bold text-[#F74518] text-center">Recherche une alternance de concepteur et développeur d'application</span>
+                        <p className="text-sm text-center text-white">"Test, fail, learn and repeat !"</p>
                     </div>
 
                     {/* LIST OF LINKS */}
-                    <ul>
-                        {/* <Link
-                            href={#}
-                        >
-                            <div className="">
-                                <p></p>
-                                <span className=""></span>
-                            </div>
-                        </Link> */}
-                    </ul>
+
+                        <ul className="w-full flex flex-col gap-2">
+
+                            {cardLinks.map((link, index) => (
+                                <li className="flex w-full justify-center" key={index}>
+                                    {link.options ? (
+                                        <div className="w-full relative flex items-center justify-center rounded-lg bg-white py-2 hover:bg-[#f74518] transition-transform duration-300">
+                                            <select
+                                                defaultValue=""
+                                                className="absolute text-base uppercase appearance-none w-full bg-transparent text-center cursor-pointer focus:outline-none inset-0 opacity-0 z-10"
+                                            >
+                                                <option value="" disabled hidden>{link.label}</option>
+                                                {link.options.map((option, i) => (
+                                                    <option key={i} value={option} className="text-black bg-white">
+                                                        {option}
+                                                    </option>
+                                                ))}
+                                            </select>
+                                            <div className="flex items-center justify-center bg-white gap-2 pointer-events-none">
+                                                <span className="text-base uppercase text-[#003C57] font-bold">{link.label}</span>
+                                                <IoMdArrowDropdown className="text-[32px] text-[#003C57]" />
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <Link
+                                            href={link.url}
+                                            className="w-full flex justify-center items-center rounded-lg bg-white text-base py-2 hover:bg-[#f74518] transition-transform duration-300"
+                                        >
+                                            <div className="flex items-center justify-center gap-2">
+                                                <p className="uppercase font-bold text-[#003C57]">{link.label}</p>
+                                                <span className="text-[20px] text-[#003C57]">{link.icon}</span>
+                                            </div>
+                                        </Link>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
                 </div>
             </div>
         </section>
