2e6469b5c6d3f61addf8f8658f073ec07072b3ec
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-13 16:46:59 +0100

    feat: left side content style added

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -43,20 +43,20 @@ export default function HomePage() {
     ]
 
     return (
-        <section>
+        <section className="flex flex-col gap-4 py-[5%] px-[5%]">
 
             {/* LEFT SIDE CONTENT */}
 
-            <h1>Bienvenue sur le portfolio d'un développeur passionné ! </h1>
-            <div>
-                <div>
-                    <p>Je m’appelle Mathieu Bourasseau.</p>
-                    <p>
+            <h1 className="text-lg font-bold text-[#003C57]">Bienvenue sur le portfolio d'un développeur passionné ! </h1>
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-col gap-4">
+                    <p className="text-sm">Je m’appelle Mathieu Bourasseau.</p>
+                    <p className="text-sm">
                         Je suis actuellement en formation de concepteur et développeur d'application pour devenir développeur full stack et en recherche
-                        <span>d'une alternance pour l'année 2026</span>
+                        <span className="text-sm font-bold text-[#F74518]">d'une alternance pour l'année 2026.</span>
                     </p>
-                    <p>Ce site web a pour objectif de vous présenter mon parcours, mes compétences, ainsi que les projets que j’ai pu réalisés.</p>
-                    <p>En espérant pouvoir se rencontrer prochainement, je vous souhaite une très bonne découverte de mon portfolio.</p>
+                    <p className="text-sm">Ce site web a pour objectif de vous présenter mon parcours, mes compétences, ainsi que les projets que j’ai pu réalisés.</p>
+                    <p className="text-sm">En espérant pouvoir se rencontrer prochainement, je vous souhaite une très bonne découverte de mon portfolio.</p>
                 </div>
                 <div>
                     <Image
@@ -67,9 +67,9 @@ export default function HomePage() {
                     />
 
                     <div>
-                        <p>Mathieu Bourasseau</p>
-                        <span>Recherche une alternance de concepteur et développeur d'application</span>
-                        <p>"Test, fail, learn and repeat !"</p>
+                        <p className="text-sm">Mathieu Bourasseau</p>
+                        <span className="text-sm font-bold text-[#F74518]">Recherche une alternance de concepteur et développeur d'application</span>
+                        <p className="text-sm">"Test, fail, learn and repeat !"</p>
                     </div>
 
                     {/* LIST OF LINKS */}
