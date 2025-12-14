9126eee3b24d7584feda2682c039ac4b566c30b1
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-14 18:16:07 +0100

    feat: activePage with 'projects' added to Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Navigation from "@/components/Navigation";
 import HomePage from "@/components/HomePage";
+import Projects from "@/components/Projects";
 import { useState } from "react";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
             <Navigation onActivePage={setActivePage} activePage={activePage} />
             <main className="flex flex-col flex-1">
                 {activePage === 'home' && <HomePage />}
+                {activePage === 'projects' && <Projects />}
             </main>
         </>
     );
