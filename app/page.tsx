0c2005b8758c844f9b2d549e40a516092af20410
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-14 17:49:52 +0100

    feat:state activePage created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Navigation from "@/components/Navigation";
 import HomePage from "@/components/HomePage";
+import { useState } from "react";
 
 export default function Home() {
+
+    const [activePage, setActivePage] = useState('home');
+
     return (
         <>
             <Navigation />
