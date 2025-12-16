83f9aa038ddebf3c9991b13358632c3028e2b8ef
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-16 12:40:38 +0100

    feat: drop-shadow style added on texts

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -63,17 +63,20 @@ export default function Projects() {
                     <article
                         key={i}
                         className="
-                            relative group/card flex flex-col overflow-hidden text-sm text-white cursor-pointer  h-[200px] rounded-lg  transition-all duration-300 ease-in-out ring-0  hover:ring-2 hover:ring-[#2C3E50] ring-inset   hover:scale-105  hover:shadow-[0_0_10px_#2C3E50,0_0_20px_rgba(44,62,80,0.5),0_0_40px_rgba(44,62,80,0.3)]
+                            relative group/card flex flex-col items-center justify-center overflow-hidden text-sm text-white cursor-pointer  h-[200px] rounded-lg  transition-all duration-300 ease-in-out ring-0  hover:ring-2 hover:ring-[#2C3E50] ring-inset   hover:scale-105  hover:shadow-[0_0_10px_#2C3E50,0_0_20px_rgba(44,62,80,0.5),0_0_40px_rgba(44,62,80,0.3)]
                         "
                     >
+                        {/* BACKGROUND OF EACH PROJECT */}
                         <div
-                            className="absolute inset-0  bg-cover bg-center bg-no-repeat brightness-75 
+                            className="absolute inset-0  bg-cover bg-center bg-no-repeat brightness-60 
                            group-hover/card:!blur-none group-hover:blur-[2px] hover:!blur-none hover:brightness-110 "
                             style={{ backgroundImage: `url(${project.img})` }}
                         >
 
+                        {/* TEXTS */}
+
                         </div>
-                        <div className="relative z-10">
+                        <div className="relative z-10 text-lg font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                             <h2 className="text-white">{project.title}</h2>
                             <span className="text-[#F74518]">{project.type}</span>
                         </div>
