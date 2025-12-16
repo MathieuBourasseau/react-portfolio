6a298f5779605bb49b4b310069d4874df97173b9
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-16 10:48:40 +0100

    feat: img and list properties added to ProjectsConfig + projects added

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,50 +1,75 @@
 import React from 'react'
 
-type ProjectsConfig= {
-  title: string;
-  type: string;
-  link: string;
+type ProjectsConfig = {
+    title: string;
+    type: string;
+    link: string;
+    img: string;
 }
 
 export default function Projects() {
 
-  const projects : ProjectsConfig = [
-    {
-      title: "Heros League",
-      type: "Front-end",
-      link: ""
-    },
-    {
-      title: "Gaming Campus",
-      type: "Front-end",
-      link: ""
-    },
-    {
-      title: "Booking app",
-      type: "Full stack - SSR",
-      link: ""
-    },
-    {
-      title: "Carambar",
-      type: "Full stack - API REST",
-      link: ""
-    },
-    {
-      title: "Coffee-shop",
-      type: "Full stack - SSR",
-      link: ""
-    },
-    {
-      title: "Chat LLM",
-      type: "Full stack - SSR",
-      link: ""
-    },
-    
-  ]
+    const projects: ProjectsConfig[] = [
+        {
+            title: "Heros League",
+            type: "Front-end",
+            img:"",
+            link: ""
+        },
+        {
+            title: "Gaming Campus",
+            type: "Front-end",
+            img:"",
+            link: ""
+        },
+        {
+            title: "Booking app",
+            type: "Full stack - SSR",
+            img:"",
+            link: ""
+        },
+        {
+            title: "Carambar",
+            type: "Full stack - API REST",
+            img:"",
+            link: ""
+        },
+        {
+            title: "Coffee-shop",
+            type: "Full stack - SSR",
+            img:"",
+            link: ""
+        },
+        {
+            title: "Chat LLM",
+            type: "Full stack - SSR",
+            img:"",
+            link: ""
+        },
 
-  return (
-    <section className="relative z-10">
-        <h1 className="text-white">Mes projets</h1>
-    </section>
-  )
+    ]
+
+    return (
+        <section className="relative z-10 py-6 flex flex-col text-center">
+            <h1 className="text-2xl text-white font-bold lg:text-3xl">Mes projets</h1>
+
+            {/* PROJECTS PART */}
+
+            <div>
+
+                {/* EACH PROJECT */}
+
+                {projects.map((project, i) => (
+                    <article 
+                        key={i}
+                        className="text-sm text-white"
+                    >
+                        <h2>{project.title}</h2>
+                        <span>{project.type}</span>
+
+                    </article>
+                ))}
+            </div>
+        </section>
+    )
 }
