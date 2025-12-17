8c4c550336522a5bcd1f1f9ee139324492619852
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-17 16:07:33 +0100

    feat: TechConfig type added + boolean available added in ProjectsConfig

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,8 +5,14 @@ type ProjectsConfig = {
     type: string;
     link: string;
     img: string;
+    items: TechConfig[],
+    available: boolean;
 }
 
+type TechConfig = {
+    icon: string;
+    tech: string;
+}
 export default function Projects() {
 
     const projects: ProjectsConfig[] = [
@@ -14,37 +20,68 @@ export default function Projects() {
             title: "Heros League",
             type: "Front-end",
             img: "/projects/heros.jpg",
-            link: "https://projet-heros.vercel.app/"
+            link: "https://projet-heros.vercel.app/",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: true,
         },
         {
             title: "Gaming Campus",
             type: "Front-end",
             img: "/projects/gaming-campus.jpg",
-            link: "https://gaming-campus-eight.vercel.app/"
+            link: "https://gaming-campus-eight.vercel.app/",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: true,
         },
         {
             title: "Carambar",
             type: "Full stack - API REST",
             img: "/projects/bonbons.jpg",
-            link: "https://carambar-front-psi.vercel.app/"
+            link: "https://carambar-front-psi.vercel.app/",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: true,
         },
         {
             title: "Booking app",
             type: "Full stack - SSR",
             img: "/projects/landscape.jpg",
-            link: "https://booking-app-production-f97c.up.railway.app/"
+            link: "https://booking-app-production-f97c.up.railway.app/",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: true,
+            
         },
         {
             title: "Coffee-shop",
             type: "Full stack - SSR",
             img: "/projects/coffee.jpg",
-            link: "#"
+            link: "#",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: false,
         },
         {
             title: "Chat LLM",
             type: "Full stack - SSR",
             img: "/projects/ia.jpg",
-            link: "#"
+            link: "#",
+            items: [ 
+                    { icon: "/skills/html.svg", tech: "html"}, 
+                    { icon: "/skills/css.svg", tech: "css" } 
+                ],
+            available: false,
         },
 
     ]
