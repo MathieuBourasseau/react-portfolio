c2ddfa588e23225ee0d9a9337be56d918a1ce5ff
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-18 17:08:28 +0100

    feat: Careerprops added

--- app/components/Career.tsx
+++ app/components/Career.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Background } from "../types/background"
 
-export default function Career() {
+type CareerProps = {
+  activeBackground: Background
+}
+
+export default function Career({ activeBackground}) {
   return (
     <div>
       <h1>Mon parcours</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Projects from "@/app/components/Projects";
 import Skills from "@/app/components/Skills";
 import Career from "@/app/components/Career";
 import { Background } from "./types/background";
-import { use, useState } from "react";
+import { useState } from "react";
 import { BackgroundBeams } from "@/app/components/ui/background-beams";
 
 export default function Home() {
@@ -17,15 +17,6 @@ export default function Home() {
     // The background to show by default is professional 
     const [activeBackground, setActiveBackground] = useState<Background>('PROFESSIONAL');
 
-
-    // Je veux qu'un de mes enfants affiche un contenu en fonction de la valeur du background : 
-        // Si background = PROFESIONNAL on affiche le parcours professionnel
-        // Si background = ACADEMIC on affiche le parcours académique
-
-        //1. Créer une variable d'état qui stocke la valeur de background
-        //2. Cette valeur est ensuite envoyer à l'enfant pour qu'il sache quoi afficher il récupère donc la props du parent
-        //3. Depuis l'enfant on modifiera cette valeur en fonction du select.
-
     return (
         <>
             <Navigation onActivePage={setActivePage} activePage={activePage} activeBackground={activeBackground} onActiveBackground={setActiveBackground} />
@@ -33,6 +24,7 @@ export default function Home() {
                 {activePage === 'home' && <HomePage />}
                 {activePage === 'projects' && <Projects />}
                 {activePage === 'skills' && <Skills /> }
+                {activePage === 'background' && <Career activeBackground={activeBackground} />}
                 <BackgroundBeams />
             </main>
         </>
