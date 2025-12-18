a5733b2debf866cc3e619b002d75038dd927a082
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-18 14:46:40 +0100

    feat: Background imported in page.tsx + activeBackground state initialized to PROFESSIONAL

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,26 @@ import HomePage from "@/app/components/HomePage";
 import Projects from "@/app/components/Projects";
 import Skills from "@/app/components/Skills";
 import Career from "@/app/components/Career";
-import { useState } from "react";
+import { Background } from "./types/background";
+import { use, useState } from "react";
 import { BackgroundBeams } from "@/app/components/ui/background-beams";
 
 export default function Home() {
 
+    // The page to display is initialized by default to "home"
     const [activePage, setActivePage] = useState('home');
+
+    // The background to show by default is professional 
+    const [activeBackground, setActiveBackground] = useState<Background>('PROFESSIONAL');
+
+
+    // Je veux qu'un de mes enfants affiche un contenu en fonction de la valeur du background : 
+        // Si background = PROFESIONNAL on affiche le parcours professionnel
+        // Si background = ACADEMIC on affiche le parcours académique
+
+        //1. Créer une variable d'état qui stocke la valeur de background
+        //2. Cette valeur est ensuite envoyer à l'enfant pour qu'il sache quoi afficher il récupère donc la props du parent
+        //3. Depuis l'enfant on modifiera cette valeur en fonction du select.
 
     return (
         <>
