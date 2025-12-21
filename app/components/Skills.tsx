3276e3e04296dd886b4f8c3ae26c552f3a8e69e6
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-21 19:21:49 +0100

    feat: contentSkills.ts created + react icons imported within it

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,15 +1,22 @@
 import Image from "next/image";
-import { IoMdFlame } from "react-icons/io";
-import { FaHandshake } from "react-icons/fa";
-import { RiTeamFill } from "react-icons/ri";
-import { FaGraduationCap } from "react-icons/fa";
-import { BsFillPatchQuestionFill } from "react-icons/bs";
-import { FaClipboardList } from "react-icons/fa";
-import { FaGear } from "react-icons/fa6";
-import React from 'react'
+
 import { motion } from "framer-motion";
 
+type ItemsConfig = {
+    label: string;
+    src: string;
+    alt: string;
+}
 
+type HardSkillsConfig = {
+    category: string;
+    items: ItemsConfig[];
+}
+
+type SoftSkillsConfig = {
+    label: string;
+    icon: React.ReactNode;
+}
 
 export default function Skills() {
 
--- app/components/contentSkills.ts
+++ app/components/contentSkills.ts
@@ -1,15 +1,8 @@
-type ItemsConfig = {
-    label: string;
-    src: string;
-    alt: string;
-}
-
-type HardSkillsConfig = {
-    category: string;
-    items: ItemsConfig[];
-}
-
-type SoftSkillsConfig = {
-    label: string;
-    icon: React.ReactNode;
-}
+import { IoMdFlame } from "react-icons/io";
+import { FaHandshake } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
+import { FaGraduationCap } from "react-icons/fa";
+import { BsFillPatchQuestionFill } from "react-icons/bs";
+import { FaClipboardList } from "react-icons/fa";
+import { FaGear } from "react-icons/fa6";
+import React from 'react'
